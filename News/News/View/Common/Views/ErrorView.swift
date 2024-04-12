4bda1696c6a4a101caaee528ad7b8deb010cba0f
Ярослав Куприянов <upakkomi@gmail.com>
2024-04-13 02:42:46 +0300

    features/error-image-update

--- News/News/View/Common/Views/ErrorView.swift
+++ News/News/View/Common/Views/ErrorView.swift
@@ -6,10 +6,13 @@
 //
 
 import SwiftUI
+import Lottie
 
 struct ErrorView: View {
 
     @ObservedObject var viewModel: ViewModel
+
+    @State private var scale = 1.0
 
     var title: String
     var action: Action
@@ -28,10 +31,23 @@ struct ErrorView: View {
                                             bottom: .zero,
                                             trailing: 100))
 
-                    Image("ErrorCat")
-                        .resizable()
-                        .imageScale(.small)
-                        .frame(width: 90, height: 100)
+                    LottieView(animation: .named("error"))
+                        .playing(loopMode: .playOnce)
+                        .shadow(color: .red, radius: 30)
+                        .frame(width: 150, height: 100)
+                        .ignoresSafeArea()
+                        .scaledToFit()
+                        .padding(.horizontal)
+                        .scaleEffect(scale)
+                        .onAppear {
+                            withAnimation(.easeInOut(duration: 0.1)) {
+                                scale = 2.1
+                            } completion: {
+                                withAnimation(.easeInOut(duration: 0.3)) {
+                                    scale = 1.0
+                                }
+                            }
+                        }
                 }
 
                 CustomButton(viewModel: viewModel,
