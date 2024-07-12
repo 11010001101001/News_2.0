0b8a7090adae681cb69bb4f619e60d061f2a738e
Ярослав Куприянов <upakkomi@gmail.com>
2024-07-12 03:35:16 +0300

    conditional view modifier done

---
+++ News/News/Helpers/Modifiers/AnswerNegative.swift
@@ -0,0 +1,43 @@
+//
+//  AnswerNegative.swift
+//  News
+//
+//  Created by Ярослав Куприянов on 12.07.2024.
+//
+
+import SwiftUI
+
+struct AnswerNegative: ViewModifier {
+    @State private var left = false
+    @State private var right = false
+    @State private var initial = false
+
+    func body(content: Content) -> some View {
+        content
+            .offset(x: left ? -5 : 0)
+            .offset(x: right ? 10 : 0)
+            .offset(x: initial ? -5 : 0)
+            .onTapGesture {
+                withAnimation {
+                    left.toggle()
+                }
+            completion: {
+                withAnimation {
+                    right.toggle()
+                }
+            completion: {
+                withAnimation(
+                    .interpolatingSpring(
+                        stiffness: 1000,
+                        damping: 5)) { initial.toggle() }
+            }
+            }
+            }
+    }
+}
+
+#Preview {
+    Circle()
+        .frame(width: 100, height: 100, alignment: .center)
+        .modifier(AnswerNegative())
+}
