3f86c730008f2d7eeb830f88c0b52ba3de888c47
Yaroslav Kupriyanov <upakkomi@gmail.com>
2024-11-17 22:43:34 +0300

    strings and assets swiftgen update

---
+++ News/News/Helpers/Sources/Strings/GeneratedStrings.swift
@@ -0,0 +1,45 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+import Foundation
+
+// swiftlint:disable superfluous_disable_command file_length implicit_return prefer_self_in_static_references
+
+// MARK: - Strings
+
+// swiftlint:disable explicit_type_interface function_parameter_count identifier_name line_length
+// swiftlint:disable nesting type_body_length type_name vertical_whitespace_opening_braces
+public enum Texts {
+  public enum Notification {
+    /// Strings.strings
+    ///   News
+    /// 
+    ///   Created by Yaroslav Kupriyanov on 17.11.2024.
+    public static func title() -> String { Texts.tr("Strings", "notification.title", fallback: "Mmmmm nice-ey")
+    }
+  }
+}
+// swiftlint:enable explicit_type_interface function_parameter_count identifier_name line_length
+// swiftlint:enable nesting type_body_length type_name vertical_whitespace_opening_braces
+
+// MARK: - Implementation Details
+
+extension Texts {
+  private static func tr(_ table: String, _ key: String, _ args: CVarArg..., fallback value: String) -> String {
+    let format = BundleToken.bundle.localizedString(forKey: key, value: value, table: table)
+    return String(format: format, locale: Locale.current, arguments: args)
+  }
+}
+
+// swiftlint:disable convenience_type
+private final class BundleToken {
+  static let bundle: Bundle = {
+    #if SWIFT_PACKAGE
+    return Bundle.main
+    #else
+    return Bundle(for: BundleToken.self)
+    #endif
+  }()
+}
+// swiftlint:enable convenience_type
+// swiftlint:enable all
