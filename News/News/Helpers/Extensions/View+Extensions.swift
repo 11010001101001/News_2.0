add35d594ebda5377b1252a172c305727e297dca
Yaroslav Kupriyanov <upakkomi@gmail.com>
2025-02-12 08:23:28 +0300

    async image ui bug fixed

--- News/News/Helpers/Extensions/View+Extensions.swift
+++ News/News/Helpers/Extensions/View+Extensions.swift
@@ -8,34 +8,7 @@
 import Foundation
 import SwiftUI
 
-// swiftlint:disable large_tuple
 extension View {
-	private var rotationCoordinates: [(x: CGFloat, y: CGFloat, z: CGFloat)] {
-		var randomNumbers = [CGFloat]()
-
-		for num in stride(from: -1.0, to: 1.0, by: 0.1) {
-			randomNumbers.append(num)
-		}
-
-		let randomX = randomNumbers.randomElement() ?? .zero
-		let randomY = randomNumbers.randomElement() ?? .zero
-		let randomZ = randomNumbers.randomElement() ?? .zero
-
-		var coordinates = [(x: CGFloat, y: CGFloat, z: CGFloat)]()
-
-		for _ in 0...10 {
-			coordinates.append((x: randomX,
-								y: randomY,
-								z: randomZ))
-		}
-
-		return coordinates
-	}
-
-	private var randomCoordinates: (x: CGFloat, y: CGFloat, z: CGFloat) {
-		rotationCoordinates.randomElement() ?? (x: 1.0, y: 1.0, z: 0.0)
-	}
-
 	func commonScaleAffect(state: Bool) -> some View {
 		return self
 			.scaleEffect((state ? 1.0 : .zero) ?? .zero)
@@ -46,7 +19,7 @@ extension View {
 		return self
 			.rotation3DEffect(
 				Angle(degrees: rotating ? -10 : 10),
-				axis: rotating ? randomCoordinates : randomCoordinates,
+				axis: rotating ? CGFloat.randomCoordinates : CGFloat.randomCoordinates,
 				anchor: .center,
 				anchorZ: 0.5,
 				perspective: rotating ? 1 : -1
@@ -141,4 +114,3 @@ extension View {
 			.any()
 	}
 }
-// swiftlint:enable large_tuple
