edca40e962e9eaef99999bf3f081b2cbb78f9857
Yaroslav Kupriyanov <upakkomi@gmail.com>
2025-02-14 01:04:47 +0300

    InnerShadowProvider update

--- News/News/Helpers/Modifiers/InnerShadowProvider.swift
+++ News/News/Helpers/Modifiers/InnerShadowProvider.swift
@@ -11,25 +11,18 @@ struct InnerShadowProvider<S: Shape>: ViewModifier {
 	@State private var radius: CGFloat = .zero
 
 	private let shape: S
-	private let angle: Angle
 	private let colors: [Color]
 	private let width: CGFloat
-	private let finalX: CGFloat
-	private let finalY: CGFloat
 
 	init(
 		shape: S = .rect(cornerRadius: Constants.cornerRadius),
-		angle: Angle = .degrees(.zero),
 		colors: [Color] = [.blue, .indigo, .red, .cyan, .blue],
 		width: CGFloat = 6
 	) {
 		self.shape = shape
-		self.angle = angle
 		self.colors = colors
 		self.width = width
 		radius = 2.0
-		finalX = CGFloat(cos(angle.radians - .pi / 2))
-		finalY = CGFloat(sin(angle.radians - .pi / 2))
 	}
 
 	func body(content: Content) -> some View {
@@ -64,11 +57,6 @@ struct InnerShadowProvider<S: Shape>: ViewModifier {
 							.mask(shape)
 					}
 				}
-					.onAppear {
-						withAnimation(.smooth(duration: 3).repeatForever(autoreverses: true)) {
-							radius = 4.0
-						}
-					}
 			)
 	}
 }
